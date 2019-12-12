922624fb8f336e771417e0d0195a3f5b91cd0399
Nichts <nichts@users.noreply.github.com>
2019-12-12 22:26:28 +0100

    Clippy and format

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -15,32 +15,30 @@ fn full_fuel_requirement(mass: u64) -> u64 {
         last_fuel_requirement = fuel_requirement(last_fuel_requirement);
         match last_fuel_requirement {
             0 => None,
-            _ => Some(last_fuel_requirement)
+            _ => Some(last_fuel_requirement),
         }
-    }).fold(total_fuel_requirement, |acc, x| acc + x)
+    })
+    .fold(total_fuel_requirement, |acc, x| acc + x)
 }
 
 fn get_modules() -> Result<Vec<u64>> {
     let file: File = File::open("input.txt")?;
     let buf_reader = BufReader::new(file);
-    let iter = buf_reader
-        .lines()
-        .map(|line| {
-            Ok(line?.parse::<u64>()?)
-        });
+    let iter = buf_reader.lines().map(|line| Ok(line?.parse::<u64>()?));
     let modules: Result<Vec<_>, _> = iter.collect();
     modules
 }
 
-pub(crate) fn calculate_fuel(modules: &Vec<u64>, fn_fuel: &dyn Fn(u64) -> u64) -> u64 {
-    modules
-        .iter()
-        .fold(0u64, |acc, &x| acc + fn_fuel(x))
+pub(crate) fn calculate_fuel(modules: &[u64], fn_fuel: &dyn Fn(u64) -> u64) -> u64 {
+    modules.iter().fold(0u64, |acc, &x| acc + fn_fuel(x))
 }
 
 fn main() -> Result<()> {
     let masses = get_modules()?;
     println!("Part 1: {}", calculate_fuel(&masses, &fuel_requirement));
-    println!("Part 2: {}", calculate_fuel(&masses,&full_fuel_requirement));
+    println!(
+        "Part 2: {}",
+        calculate_fuel(&masses, &full_fuel_requirement)
+    );
     Ok(())
 }
